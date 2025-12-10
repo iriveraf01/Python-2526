5e8f60131ff09e915d50230864f3b1c60955942b
iriveraf01 <iriveraf01@suarezdefigueroa.es>
2025-12-10 21:06:12 +0100

    Funciona

--- AD/Bot/acceso_datos.py
+++ AD/Bot/acceso_datos.py
@@ -1,135 +1,139 @@
-import pandas as pd
 from influxdb import InfluxDBClient
-
+import pandas as pd
 
 HOST = '10.0.0.8'
-PORT = 8886
+PUERTO = 8886
 USER = 'pythonread'
 PASS = 'read1234'
-DB = 'agro_sensex'
+DATABASE = 'agro_sensex'
+
 
 def ping():
-    client = InfluxDBClient(HOST, PORT, USER, PASS, DB)
+    client = InfluxDBClient(host=HOST, port=PUERTO, username=USER, password=PASS)
     print("Prueba de conexión")
     ping_result = client.ping()
+
     if ping_result:
         print(ping_result)
     else:
         print("No conectado")
 
 def consulta_bbdd():
-    client = InfluxDBClient(HOST, PORT, USER, PASS, DB)
-    print("\nMostrando las bases de datos")
+    client = InfluxDBClient(host=HOST, port=PUERTO, username=USER, password=PASS)
+    print("Mostrando bases de datos...\n")
     databases = client.get_list_database()
-    for i in databases:
-        print(i['name'])
+
+    for db in databases:
+        print(db['name'])
 
 def consultar_tablas():
-    client = InfluxDBClient(HOST, PORT, USER, PASS, DB)
-    print(f"\nMostrando tablas de la BD ({DB})")
-    respuesta = client.query("SHOW MEASUREMENTS")
-    points = list(respuesta.get_points())
-    print(f"Existen {len(points)} measurements")
-    for i in points:
-        print(i['name'])
+    client = InfluxDBClient(host=HOST, port=PUERTO, username=USER, password=PASS)
+    client.switch_database(DATABASE)
+
+    print("Mostrando tablas de la base de datos...\n")
+
+    respuesta = client.query('SHOW MEASUREMENTS')
+    points = respuesta.get_points()
+
+    for point in points:
+        print(point['name'])
 
 def consultar_tag_key():
-    client = InfluxDBClient(HOST, PORT, USER, PASS, DB)
-    print("\nMostrando columnas de tipo TAG")
+    client = InfluxDBClient(host=HOST, port=PUERTO, username=USER, password=PASS)
+    client.switch_database(DATABASE)
+
+    print("Mostrando tag keys...\n")
+
     respuesta = client.query('SHOW TAG KEYS FROM "%"')
     points = list(respuesta.get_points())
-    print(f"Existen {len(points)} columnas de tipo TAG en la tabla %")
-    for i in points:
-        print(i)
+
+    for point in points:
+        print(point)
 
 def consultar_tag_values():
-    client = InfluxDBClient(HOST, PORT, USER, PASS, DB)
-    print("\nMostrando valores únicos de la columna entity_id")
+    client = InfluxDBClient(host=HOST, port=PUERTO, username=USER, password=PASS)
+    client.switch_database(DATABASE)
+
+    print("Mostrando valores únicos de la columna entity_id...\n")
+
     respuesta = client.query('SHOW TAG VALUES FROM "%" WITH KEY = "entity_id"')
     points = list(respuesta.get_points())
-    print(f"Existen {len(points)} valores únicos")
-    for i in points:
-        print(i)
 
-def consultar_fields():
-    client = InfluxDBClient(HOST, PORT, USER, PASS, DB)
-    print("\nMostrando las columnas tipo field de %")
-    respuesta = client.query('SHOW FIELD KEYS FROM "%"')
-    points = list(respuesta.get_points())
-    print(f"Existen {len(points)} valores únicos")
-    for i in points:
-        print(i)
+    for point in points:
+        print(point)
 
 def prueba_consulta():
-    sensor = 'agsex_sdf_huerto_lht65n_humedad'
-    client = InfluxDBClient(HOST, PORT, USER, PASS, DB)
+    sensor = "agsex_sdf_huerto_lht65n_humedad"
+    client = InfluxDBClient(host=HOST, port=PUERTO, username=USER, password=PASS)
+    client.switch_database(DATABASE)
+
+    print("Mostrando los datos de las 2 últimas horas del sensor", sensor, "...\n")
     respuesta = client.query(
         f'SELECT "value" FROM "%" '
-        f'WHERE "entity_id" = \'{sensor}\' '
+        f'WHERE "entity_id" = \'{sensor}\''
         f'AND time > now() - 2h'
     )
+
     points = list(respuesta.get_points())
     print(list(points[0].keys()))
-    for i in points:
-        print(list(i.values()))
+
+    for point in points:
+        print(list(point.values()))
+
 
 def diario_huerto_humedad():
-    client = InfluxDBClient(HOST, PORT, USER, PASS, DB)
-    sensor = 'agsex_sdf_huerto_lht65n_humedad'
+    client = InfluxDBClient(host=HOST, port=PUERTO, username=USER, password=PASS)
+    client.switch_database(DATABASE)
+    sensor = "agsex_sdf_huerto_lht65n_humedad"
 
     query = (
-        'SELECT MEAN("value") AS mean, MIN("value") AS min, MAX("value") AS max '
-        'FROM "value" '
-        f'WHERE "entity_id" = \'{sensor}\' '
-        'AND time > now() - 2d '
-        'GROUP BY time(1d)'
+        f'SELECT MEAN("value"), MIN("value"), MAX("value") FROM "%"'
+        f'WHERE "entity_id" = \'{sensor}\''
+        f'AND time >= now() - 2d '
+        f'GROUP BY time(1d)'
     )
 
     respuesta = client.query(query)
     points = list(respuesta.get_points())
 
     if not points:
-        return None
-    else:
-        return {
-            "mean": points[1]["mean"],
-            "min": points[1]["min"],
-            "max": points[1]["max"]
-        }
+        return {}
+    
+    point = points[1]
+    datos = dict()
 
+    datos["media"] = round(point['mean'],1)
+    datos["minimo"] = round(point['min'],1)
+    datos["maximo"] = round(point['max'],1)
+
+    return datos
 
 def diario_huerto_temperatura():
-    client = InfluxDBClient(HOST, PORT, USER, PASS, DB)
-    sensor = 'agsex_sdf_huerto_lht65n_temperatura'
+    client = InfluxDBClient(host=HOST, port=PUERTO, username=USER, password=PASS)
+    client.switch_database(DATABASE)
+    sensor = "agsex_sdf_huerto_lht65n_temperatura"
 
     query = (
-        'SELECT MEAN("value") AS mean, MIN("value") AS min, MAX("value") AS max '
-        'FROM "value" '
-        f'WHERE "entity_id" = \'{sensor}\' '
-        'AND time > now() - 2d '
-        'GROUP BY time(1d)'
+        f'SELECT MEAN("value"), MIN("value"), MAX("value") FROM "°C"'
+        f'WHERE "entity_id" = \'{sensor}\''
+        f'AND time >= now() - 2d '
+        f'GROUP BY time(1d)'
     )
 
     respuesta = client.query(query)
     points = list(respuesta.get_points())
 
     if not points:
-        return None
-    else:
-        return {
-            "mean": points[1]["mean"],
-            "min": points[1]["min"],
-            "max": points[1]["max"]
-        }
+        return {}
+    
+    point = points[1]
+    datos = dict()
 
+    datos["media"] = round(point['mean'],1)
+    datos["minimo"] = round(point['min'],1)
+    datos["maximo"] = round(point['max'],1)
+
+    return datos
 
 if __name__ == '__main__':
-    # ping()
-    # consulta_bbdd()
-    consultar_tablas()
-    # consultar_tag_key()
-    # consultar_tag_values()
-    # consultar_fields()
-    # prueba_consulta()
-    # diario_huerto_humedad()
-    # diario_huerto_temperatura()
+    print(diario_huerto_temperatura())
--- AD/Bot/bot.py
+++ AD/Bot/bot.py
@@ -1,75 +1,74 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 import acceso_datos
 import logging
 from config import TOKEN
+# ====================
+# Configuración básica
+# ====================
+logging.basicConfig(format='%(asctime)s- %(name)s $(levelname)s - %(message)s', level=logging.INFO)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-
-def formatear_mensaje(humedad: dict, temp: dict):
-    return (
-        f"Humedad relativa del ambiente (%)\n"
-        f"  - Media: {humedad['mean']}\n"
-        f"  - Min: {humedad['min']}\n"
-        f"  - Max: {humedad['max']}\n"
-        f"Temperatura relativa del ambiente (°C)\n"
-        f"  - Media: {temp['mean']}\n"
-        f"  - Min: {temp['min']}\n"
-        f"  - Max: {temp['max']}\n"
+def formatear_mensaje(humedad, temp):
+    return(
+        f'Humedad relativa del ambiente (%)\n'
+        f'  - Media: {humedad['media']}\n'
+        f'  - Min: {humedad['minimo']}\n'
+        f'  - Max: {humedad['maximo']}\n'
+        f'Temperatura relativa del ambiente (°C)\n'
+        f'  - Media: {temp['media']}\n'
+        f'  - Min: {temp['minimo']}\n'
+        f'  - Max: {temp['maximo']}\n'
     )
 
 def validar_ubicacion(args: list[str]):
-    if not args or len(args) > 1:
-        return None
-    ubic = args[0].lower()
-    return ubic if ubic in ['huerto', 'invernadero'] else None
+    if not args or len(args) > 1 or args[0].lower() not in ['huerto', 'invernadero']:
+        return ""
+    else:
+        return args[0].lower()
 
+# =======================
+# MANEJADORES DE COMANDOS
+# =======================
 
-async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = (
+# Comando menu
+async def menu_handler(update: Update, context:ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    text = {
         "Bot de Monitoreo Ambiental\n\n"
-        "Comandos disponibles (requieren ubicación: 'huerto' o 'invernadero'):\n"
+        "Comandos diponibles (requieren ubicación: 'huerto' o 'invernadero'):\n"
         "/actual [ubicacion]: Muestra valores actuales\n"
         "/diario [ubicacion]: Muestra resumen del día anterior\n"
         "/semanal [ubicacion]: Muestra resumen de la semana\n"
         "/consejo [ubicacion]: Consejo de Gemini"
-    )
-    await update.message.reply_text(text)
+    }
+    await context.bot.send_message(chat_id=chat_id, text=text)
 
-
+# Comando /diario humedad
+# Comando /diario invernadero
 async def diario_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     ubicacion = validar_ubicacion(context.args)
-
     if not ubicacion:
         await context.bot.send_message(chat_id=chat_id, text="Ubicación no válida. Usa huerto o invernadero.")
-        return
-
-    ayer = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
-
-    await context.bot.send_message(chat_id=chat_id, text=f"Consultando datos del {ubicacion} de {ayer}...")
-
-    if ubicacion == "huerto":
-        datos = acceso_datos.diario_huerto_humedad()
-        datos_temp = acceso_datos.diario_huerto_temperatura()
-    else:  # invernadero
-        datos = acceso_datos.diario_invernadero_humedad()
-        datos_temp = acceso_datos.diario_invernadero_temperatura()
-
-    texto = formatear_mensaje(datos, datos_temp)
-    await context.bot.send_message(chat_id=chat_id, text=texto)
-
+    else:
+        hoy = date.today()
+        ayer = hoy - timedelta(days=1)
+        fecha = ayer.strftime('%Y-%m-%d')
+        await context.bot.send_message(chat_id=chat_id, text=f"Consultando datos del {ubicacion} de {fecha}")
+        if ubicacion == "huerto":
+            datos = acceso_datos.diario_huerto_humedad()
+            datos_temp = acceso_datos.diario_huerto_temperatura()
+            texto = formatear_mensaje(datos, datos_temp)
+            await context.bot.send_message(chat_id=chat_id, text=texto)
 
 def main():
     appbot = ApplicationBuilder().token(TOKEN).build()
 
-    appbot.add_handler(CommandHandler("menu", menu_handler))
     appbot.add_handler(CommandHandler("diario", diario_handler))
-
+    appbot.add_handler(CommandHandler("menu", menu_handler))
     appbot.run_polling()
-
 
 if __name__ == '__main__':
     main()
