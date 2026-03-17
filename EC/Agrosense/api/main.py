906f3e35cb49bfefa85584ee0695d05dd3c441be
iriveraf01 <iriveraf01@suarezdefigueroa.es>
2026-03-17 21:21:48 +0100

    clase

--- EC/Agrosense/api/main.py
+++ EC/Agrosense/api/main.py
@@ -16,3 +16,18 @@ app.middleware(
 def get_plantas():
     plantas = cargar_plantas()
     return list(p.model_dump() for p in plantas.values())
+
+@app.post("/plantas")
+def post_planta(planta: PlantaCreate):
+    crear_planta(planta)
+    return {"message": "Planta creada correctamente"}
+
+@app.put("/plantas/{planta_id}")
+def put_planta(planta_id: str, planta_data: PlantaCreate):
+    actualizar_planta(planta_id, planta_data)
+    return {"message": "Planta actualizada correctamente"}
+
+@app.delete("/plantas/{planta_id}")
+def delete_planta(planta):
+    eliminar_planta(planta)
+    return {"message": "Planta eliminada correctamente"}
